b9c2be06ab0346799873a5a5a8903394114c8ba4
chub <2cs.public@gmail.com>
2015-11-15 09:59:26 +0100

    made it into an animal sounds app. hello world btw

--- app/src/main/java/chub/helloworld/MainActivity.java
+++ app/src/main/java/chub/helloworld/MainActivity.java
@@ -1,5 +1,6 @@
 package chub.helloworld;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,51 +9,111 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private ImageButton squirrelButton, crowButton, donkeyButton, chickenButton, pigButton, snakeButton, sheepButton, wolfButton;
+
+    private MediaPlayer crowSound, chickenSound, pigSound, squirrelSound, donkeySound, snakeSound, sheepSound, wolfSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        squirrelButton = (ImageButton) findViewById(R.id.squirrelButton);
+        donkeyButton = (ImageButton) findViewById(R.id.donkeyButton);
+        snakeButton = (ImageButton) findViewById(R.id.snakeButton);
+        sheepButton = (ImageButton) findViewById(R.id.sheepButton);
+        wolfButton = (ImageButton) findViewById(R.id.wolfButton);
+        chickenButton = (ImageButton) findViewById(R.id.chickenButton);
+        pigButton = (ImageButton) findViewById(R.id.pigButton);
+        crowButton = (ImageButton) findViewById(R.id.crowButton);
+        loadSounds();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
+        squirrelButton.setOnClickListener(new View.OnClickListener()
         {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v)
+            {
+                squirrelSound.start();
+            }
+        });
+
+        donkeyButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                donkeySound.start();
+            }
+        });
+
+        snakeButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                snakeSound.start();
+            }
+        });
+
+        sheepButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                sheepSound.start();
+            }
+        });
+
+        wolfButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                wolfSound.start();
+            }
+        });
+
+        chickenButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                chickenSound.start();
+            }
+        });
+
+        pigButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                pigSound.start();
+            }
+        });
+
+        crowButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                crowSound.start();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    private void loadSounds()
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        crowSound = MediaPlayer.create(this, R.raw.crow);
+        chickenSound = MediaPlayer.create(this, R.raw.chicken);
+        pigSound = MediaPlayer.create(this, R.raw.pig);
+        squirrelSound = MediaPlayer.create(this, R.raw.squirrel);
+        donkeySound = MediaPlayer.create(this, R.raw.donkey);
+        snakeSound = MediaPlayer.create(this, R.raw.snake);
+        sheepSound = MediaPlayer.create(this, R.raw.sheep);
+        wolfSound = MediaPlayer.create(this, R.raw.wolf);
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
